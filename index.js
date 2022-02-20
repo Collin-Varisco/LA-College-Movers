d2ca032662257fe1b92302138b6977c741d379d2
Collin Varisco <c00304484@louisiana.edu>
2022-02-19 20:44:27 -0600

    Used faker.js to randomly generate 300 clients and jobs

--- index.js
+++ index.js
@@ -6,9 +6,79 @@ const server = require("http").Server(app);
 const ejs = require('ejs');
 server.listen(5000, 'localhost');
 
+// Generate client and job information
+const { faker } = require('@faker-js/faker');
+
+function getRandomMonthInt(month){
+  var m = 0;
+  switch (month) {
+    case 'Jan':
+      m = 1;
+      break;
+    case 'Feb':
+      m = 2;
+      break;
+    case 'Mar':
+      m = 3;
+      break;
+    case 'Apr':
+      m = 4;
+      break;
+    case 'May':
+      m = 5;
+      break;
+    case 'Jun':
+      m = 6;
+      break;
+    case 'Jul':
+      m = 7;
+      break;
+    case 'Aug':
+      m = 8;
+      break;
+    case 'Sep':
+      m = 9;
+      break;
+    case 'Oct':
+      m = 10;
+      break;
+    case 'Nov':
+      m = 11;
+      break;
+    case 'Dec':
+      m = 12;
+      break;
+    default:
+      m = 1;
+  }
+  return m;
+}
+
+function getAddressNumber(){
+  return Math.floor(Math.random() * 750);
+}
+
+function emailService(){
+  var services = ["@gmail.com", "@outlook.com", "@yahoo.com"];
+  var min = Math.ceil(0);
+  var max = Math.floor(3);
+  var rand = Math.floor(Math.random() * (max - min) + min);
+  return services[rand];
+}
+
+function LaCity(){
+  var cities = ["New Orleans", "Baton Rouge", "Shreveport", "Metairie", "Lafayette", "Lake Charles", "Kenner", "Bossier City", "Monroe", "Alexandria", "Houma", "Prairieville"];
+  var max = Math.floor(cities.length - 1);
+  var min = Math.ceil(0);
+  var rand = Math.floor(Math.random() * (max - min) + min);
+  return cities[rand];
+}
+
 app.set("views", "./views");
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
+
+
 
 // create connection to db
 const connection = mysql.createConnection({
@@ -17,6 +87,48 @@ const connection = mysql.createConnection({
   password: 'password',
   database: 'MovingDB'
 });
+
+function randomFranchise(){
+  var max = Math.floor(4);
+  var min = Math.ceil(1);
+  var rand = Math.floor(Math.random() * (max - min) + min);
+  return rand;
+}
+
+var job_id = 1;
+var client_id = job_id + 1000;
+for(var i = 0; i < 300; i++){
+  var fName = faker.name.firstName();
+  var lName = faker.name.lastName();
+  var number = faker.phone.phoneNumberFormat();
+  var originalStreet = getAddressNumber() + " " + faker.address.streetName();
+  var destinationStreet = getAddressNumber() + " " + faker.address.streetName();
+  var date = faker.date.between("2019-01-01", "2022-05-15").toString().split(" ");
+  var month = getRandomMonthInt(date[1]);
+  var email = fName+lName+emailService();
+  var day = parseInt(date[2]);
+  var year = parseInt(date[3]);
+  var originalCity = LaCity();
+  var destinationCity = LaCity();
+
+  connection.query(
+    'INSERT INTO ClientInfo (ClientID,PhoneNumber,Email,Fname,Lname) VALUES(?, ?, ?, ?, ?)',
+    [client_id, number, email, fName, lName],
+    (error, results) => {
+      if (error) { console.log(error) };
+      console.log("Client Added");
+    });
+
+    connection.query(
+      'INSERT INTO JobInfo (JobID,Day,Month,jYear,ClientID,OriginalCity,OriginalStreetAddress,DestinationCity,DestinationStreetAddress,FranchiseID) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+      [job_id, day, month, year, client_id, originalCity, originalStreet, destinationCity, destinationStreet, randomFranchise()],
+      (error, results) => {
+        if (error) { console.log(error) };
+        console.log("Job Added");
+    });
+  job_id = job_id + 1;
+  client_id = client_id + 1;
+}
 
 function getEmployeeInfo(){
   connection.query(
